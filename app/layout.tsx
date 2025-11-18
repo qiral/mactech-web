8787cb735eadf3e13d623c5bd8faecf2308a2f41
metharda <ahmetardaorak@gmail.com>
2025-11-18 05:18:28 +0300

    corrected sponsors,speaker and schedule

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
+import { Geist , Geist_Mono} from 'next/font/google'
 import './globals.css'
 
-const _geist = Geist({ subsets: ["latin"] });
-
+const _geist = Geist({ 
+  subsets: ["latin"],
+  variable: "--font-sans"
+});
+const _geistMono = Geist_Mono({ 
+  subsets: ["latin"],
+  variable: "--font-mono"
+});
 
 export const metadata: Metadata = {
   title: 'MacTech Zirvesi',
@@ -26,9 +31,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${_geist.className} font-sans antialiased`}>
+      <body className={`${_geist.variable} ${_geistMono.variable} font-sans antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
